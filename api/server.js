9a9d5bf52b74d4464645461841435788451c40d0
Jenner Nicodemos <jenner.nicodemos@gmail.com>
2020-12-16 12:20:06 -0300

    Incluir comentários em Postagens - Parte 3

--- api/server.js
+++ api/server.js
@@ -184,7 +184,10 @@ app.get('/imagens/:imagem', function(req, res){
 
 // PUT by ID (update)
 app.put('/api/:id', function(req, res){
-    db.open( function(err, mongoclient){
+    
+    res.send('rota para atualização')
+    
+    /*db.open( function(err, mongoclient){
         mongoclient.collection('postagens', function(err, collection){
             collection.update(
                 { _id : objectId(req.params.id) },
@@ -200,7 +203,7 @@ app.put('/api/:id', function(req, res){
                 }
             )
         })
-    })
+    }) */
 })
 
 // DELETE by ID (remover)
