3d7e9df7eea02d20d4b635ae09d6e321cf354865
Jenner Nicodemos <jenner.nicodemos@gmail.com>
2020-12-16 11:03:45 -0300

    Incluir comentários em Postagens - Parte 1

--- api/server.js
+++ api/server.js
@@ -31,13 +31,52 @@ app.get('/', function(req, res){
     // res.send({msg: 'Olá'}) 
 })
 
+/*app.post('/api', function(req, res){
+     
+    res.setHeader("Access-Control-Allow-Origin", "*");
+ 
+    var date = new Date();
+    var time_stamp = date.getTime();
+ 
+    var url_imagem = time_stamp + '_' + req.files.arquivo.originalFilename;
+    var path_origem = req.files.arquivo.path;
+    var path_destino = './uploads/' + url_imagem;
+ 
+    fs.rename(path_origem, path_destino, function(err){
+        if(err){
+            res.status(500).json({ error : err });
+            return;
+        }
+ 
+        var dados = {
+            url_imagem: url_imagem,
+            titulo: req.body.titulo
+        }
+ 
+        db.open(function(err, mongoclient){
+            mongoclient.collection('postagens', function(err, collection){
+                collection.insert(dados, function(err, records){
+                    if(err){
+                        res.json({'status': 'erro'});
+                    }else{
+                        res.json({'status': 'inclusao realizada com sucesso'});
+                    }
+                    mongoclient.close();
+                });
+            });
+        });
+ 
+    });
+});*/
+
+
 // POST (create)
 app.post('/api', function(req, res){
 
     res.setHeader("Access-Control-Allow-Origin", "*")
 
     let date = new Date()
-    time_stamp = date.getTime()
+    let time_stamp = date.getTime()
     let url_imagem = time_stamp + '_' + req.files.arquivo.originalFilename
 
     console.log(req.files)
